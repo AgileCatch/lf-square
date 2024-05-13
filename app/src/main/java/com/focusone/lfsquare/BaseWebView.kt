0599fba63688e2041aa80ab75fc3c487cfa2f7fa
김영현 <agilecatch@gmail.com>
2024-05-13 14:25:58 +0900

    [feat] : webView 안에서 다른 url 불러오기

--- app/src/main/java/com/focusone/lfsquare/BaseWebView.kt
+++ app/src/main/java/com/focusone/lfsquare/BaseWebView.kt
@@ -1,13 +1,20 @@
 package com.focusone.lfsquare
 
 import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.util.AttributeSet
+import android.util.Log
 import android.webkit.CookieManager
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import java.net.URISyntaxException
 
 class BaseWebView : WebView {
     companion object {
@@ -85,11 +92,154 @@ class BaseWebView : WebView {
 
     }
 
-    private inner class MyWebChromeClient : WebChromeClient() {
+    private inner class MyWebViewClient : WebViewClient(){
+        @Deprecated("Deprecated in Java")
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            Log.e(TAG, "shouldOverrideUrlLoading : $url")
+            try {
+                if (url.startsWith("about:blank")) {
+                    Log.e(TAG, "about:blank")
+                    return true
+                } else if (url.startsWith("tel:")) {
+                    val intent = Intent(Intent.ACTION_DIAL, Uri.parse(url))
+                    mContext!!.startActivity(intent)
+                    return true
+                } else if (url.startsWith("mailto:")) {
+                    val eMail = url.replace("mailto", "")
+                    val intent = Intent(Intent.ACTION_SEND)
+                    intent.setType("plain/text")
+                    intent.putExtra(Intent.EXTRA_EMAIL, eMail)
+                    mContext!!.startActivity(intent)
+                    return true
+                } else if (url.startsWith("intent:kakao") || url.startsWith("kakao")) {
+                    try {
+                        val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+                        val existPackage = mContext!!.packageManager.getLaunchIntentForPackage(
+                            intent.getPackage()!!
+                        )
+                        if (existPackage != null) {
+                            mContext!!.startActivity(intent)
+                        } else {
+                            val marketIntent = Intent(Intent.ACTION_VIEW)
+                            marketIntent.setData(Uri.parse("market://details?id=" + intent.getPackage()))
+                            mContext!!.startActivity(marketIntent)
+                        }
+                    } catch (e: Exception) {
+                        Log.d(
+                            TAG,
+                            "Bad URI " + url + ":" + e.message
+                        )
+                        return false
+                    }
+                    return true
+                } else if (!url.startsWith("http://") &&
+                    !url.startsWith("https://") &&
+                    !url.startsWith("javascript:")
+                ) {
+                    var intent: Intent? = null
+                    intent = try {
+                        // 딥링크 스키마 확인
+                        Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
 
+                        // for test
+                        //WebSettings settings2 = getSettings();
+                        //QLog.e(TAG, ">>>>> UserAgent : " + settings2.getUserAgentString());
+                    } catch (ex: URISyntaxException) {
+                        Log.e(
+                            TAG,
+                            "[error] Bad request uri format : [" + url + "] =" + ex.message
+                        )
+                        return false
+                    }
+
+                    //
+                    // 가맹점별로 원하시는 방식으로 사용하시면 됩니다.
+                    // market URL
+                    // market://search?q="+packageNm => packageNm을 검색어로 마켓 검색 페이지 이동
+                    // market://search?q=pname:"+packageNm => packageNm을 패키지로 갖는 앱 검색 페이지 이동
+                    // market://details?id="+packageNm => packageNm 에 해당하는 앱 상세 페이지로 이동
+                    //
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+                        if (intent?.let {
+                                mContext!!.packageManager.resolveActivity(
+                                    it,
+                                    0
+                                )
+                            } == null
+                        ) { // 폰에 앱 미설치 된 경우
+                            // 스키마에 포함된 패키지명 확인
+                            val pkgName = intent?.getPackage()
+                            if (pkgName != null) {
+                                val uri = Uri.parse("market://search?q=pname:$pkgName")
+                                intent = Intent(Intent.ACTION_VIEW, uri)
+                                mContext!!.startActivity(intent)
+                            }
+                        } else { // 폰에 앱 설치된 경우
+                            val uri = Uri.parse(intent.getDataString())
+                            intent = Intent(Intent.ACTION_VIEW, uri)
+                            mContext!!.startActivity(intent)
+                        }
+                    } else {
+                        try {
+                            mContext!!.startActivity(intent)
+                        } catch (e: ActivityNotFoundException) {
+                            val uri = Uri.parse("market://search?q=pname:" + (intent?.getPackage()))
+                            intent = Intent(Intent.ACTION_VIEW, uri)
+                            mContext!!.startActivity(intent)
+                        }
+                    }
+                } else {
+                    view.loadUrl(url)
+                    return false
+                }
+                //[END INICISPAY PG사 코드] /////////////////////////////////////////////////////////
+            } catch (e: Exception) {
+                //e.printStackTrace();
+                return false
+            }
+            return true
+        }
+
+        //페이지 로딩 시작
+        override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            Log.e(TAG, "onPageStarted URL : $url")
+            if (favicon != null) {
+                // favicon이 null이 아닌 경우에 대한 처리
+            } else {
+                // favicon이 null인 경우에 대한 처리
+            }
+        }
+
+        //오류 처리
+        @Deprecated("Deprecated in Java")
+        override fun onReceivedError(
+            view: WebView,
+            errorCode: Int,
+            description: String,
+            failingUrl: String
+        ) {
+            super.onReceivedError(view, errorCode, description, failingUrl)
+            Log.e(TAG, "onReceivedError : $failingUrl")
+
+            //loadUrl(URL_ERROR);
+        }
+
+        //페이지 로딩 완료
+        override fun onPageFinished(view: WebView, url: String) {
+            super.onPageFinished(view, url)
+            Log.e(TAG, "onPageFinished : $url")
+
+            //[1] 앱 최초 기동 유무를 확인 -> MainActivity. stopAnimation() 에서 처리
+
+            // 웹뷰의 RAM과 영구 저장소 사이에 쿠키 강제 동기화 수행 함.
+            CookieManager.getInstance().flush()
+
+            //testCode();
+        }
     }
 
-    private inner class MyWebViewClient : WebViewClient(){
+    private inner class MyWebChromeClient : WebChromeClient() {
 
     }
 }
