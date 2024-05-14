b9aba58ef7f853937a92862eda23fa523d2335e7
김영현 <agilecatch@gmail.com>
2024-05-14 11:38:50 +0900

    [feat] : 뒤로가기 버튼 클릭시 종료

--- app/src/main/java/com/focusone/lfsquare/MainActivity.kt
+++ app/src/main/java/com/focusone/lfsquare/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.focusone.lfsquare
 
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,21 +12,54 @@ import com.focusone.lfsquare.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var backPressedForFinish: BackPressedForFinish
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        backPressedForFinish = BackPressedForFinish(this)
 
         initView()
 
     }
 
-    companion object{
-        const val TAG ="MainActivity"
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean = with(binding) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            val msg = ">>>>> canGoBack: [${webView.url}]"
+            Log.e(TAG, msg)
+            val nIndex = 2
+            val historyList = webView.copyBackForwardList()
+            var mallMainUrl = ""
+            val webHistoryItem = historyList.getItemAtIndex(nIndex)
+            if (webHistoryItem != null) {
+                mallMainUrl = webHistoryItem.url
+            }
+            if (webView.url.equals(mallMainUrl, ignoreCase = true)) {
+                val backBtn: BackPressedForFinish = getBackPressedClass()
+                backBtn.onBackPressed()
+            } else {
+                webView.goBack() // 뒤로가기
+            }
+        } else if (keyCode == KeyEvent.KEYCODE_BACK && !webView.canGoBack()) {
+            val backBtn: BackPressedForFinish = getBackPressedClass()
+            backBtn.onBackPressed()
+        } else {
+            return super.onKeyDown(keyCode, event)
+        }
+        return true
+    }
+
+    private fun getBackPressedClass(): BackPressedForFinish {
+        return backPressedForFinish
     }
 
     private fun initView()= with(binding) {
 //        var baseUrl = BuildConfig.MAIN_URL
         webView.loadUrl("https://www.lfsquare.co.kr/membership")
 //        Log.d(TAG, "MAIN_URL: ${BuildConfig.MAIN_URL}")
+    }
+
+    companion object{
+        const val TAG ="MainActivity"
     }
 }
