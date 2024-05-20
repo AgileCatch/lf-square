4477552e9cdbdfc1ade83a30f88391658d058f9d
김영현 <agilecatch@gmail.com>
2024-05-20 13:25:53 +0900

    [feat] 바텀앱바 구현

--- app/src/main/java/com/focusone/lfsquare/MainActivity.kt
+++ app/src/main/java/com/focusone/lfsquare/MainActivity.kt
@@ -4,6 +4,7 @@ import ShakeDetector
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
+import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
 import com.focusone.lfsquare.databinding.ActivityMainBinding
 import com.focusone.lfsquare.util.BackPressedForFinish
@@ -12,12 +13,11 @@ class MainActivity : AppCompatActivity() {
 
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var backPressedForFinish: BackPressedForFinish
-    private lateinit var mWebView: BaseWebView
     private lateinit var shakeDetector: ShakeDetector
 
     companion object {
         const val TAG = "MainActivity"
-        const val MAIN_URL= BuildConfig.MAIN_URL
+        const val MAIN_URL = BuildConfig.MAIN_URL
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,12 +27,16 @@ class MainActivity : AppCompatActivity() {
         backPressedForFinish = BackPressedForFinish(this)
 
         initView()
-
+        //바코드 세이크시 팝업
         shakeDetector = ShakeDetector(this) {
             showBarcodePopup()
         }
-
+        //버튼 셋업
+        setupButtons()
+        //바텀앱바 셋업
+        setupScrollListener()
     }
+
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean = with(binding) {
         if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
@@ -67,6 +71,8 @@ class MainActivity : AppCompatActivity() {
     private fun initView() = with(binding) {
         webView.loadUrl(MAIN_URL)
         Log.d(TAG, "baseUrl: ${BuildConfig.MAIN_URL}")
+
+
     }
 
     private fun showBarcodePopup() {
@@ -74,6 +80,83 @@ class MainActivity : AppCompatActivity() {
         binding.webView.evaluateJavascript(jsCodeBarcodePopup, null)
     }
 
+    private fun setupButtons() = with(binding) {
+        btnBack.setOnClickListener {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            }
+        }
+
+        btnForward.setOnClickListener {
+            if (webView.canGoForward()) {
+                Log.d(TAG, "Moving forward")
+                webView.goForward()
+            } else {
+                Log.d(TAG, "No next page to go forward to")
+            }
+        }
+
+        btnRefresh.setOnClickListener {
+            webView.reload()
+        }
+
+        btnHome.setOnClickListener {
+            webView.loadUrl(MAIN_URL)
+        }
+
+        btnBarcode.setOnClickListener {
+            showBarcodePopup()
+        }
+    }
+
+    private fun setupScrollListener() = with(binding) {
+        webView.viewTreeObserver.addOnScrollChangedListener(object :
+            ViewTreeObserver.OnScrollChangedListener {
+            private var lastScrollY = 0
+            private var lastScrollDirection = 0 // 1: up, -1: down
+
+            override fun onScrollChanged() {
+                val currentScrollY = webView.scrollY
+                val scrollDelta = currentScrollY - lastScrollY
+
+                if (scrollDelta > 0 && lastScrollDirection != -1) {
+                    lastScrollDirection = -1
+                    hideBottomAppBar()
+                } else if (scrollDelta < 0 && lastScrollDirection != 1) {
+                    lastScrollDirection = 1
+                    showBottomAppBar()
+                }
+
+                lastScrollY = currentScrollY
+            }
+        })
+    }
+
+    private fun hideBottomAppBar() = with(binding) {
+        apply {
+            bottomAppBar.animate()
+                .translationY(binding.bottomAppBar.height.toFloat())
+                .setDuration(300)
+                .start()
+            btnBarcode.animate()
+                .translationY(binding.bottomAppBar.height.toFloat())
+                .setDuration(300)
+                .start()
+        }
+    }
+
+    private fun showBottomAppBar() = with(binding) {
+        apply {
+            bottomAppBar.animate()
+                .translationY(0f)
+                .setDuration(300)
+                .start()
+            btnBarcode.animate()
+                .translationY(0f)
+                .setDuration(300)
+                .start()
+        }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
